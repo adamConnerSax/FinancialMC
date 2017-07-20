cdcee50fd9886eb1f2d2523dbef743e2a1df1dc7
adamConnerSax <adam_conner_sax@yahoo.com>
2017-07-19 21:02:34 -0400

    Working on prob monads from monad-bayes paper

--- src/FinancialMC/Core/Probability.hs
+++ src/FinancialMC/Core/Probability.hs
@@ -6,11 +6,28 @@ module Probability
 import Data.Random as R
 import Data.Random.RVar as R
 import Data.Random.Distribution.Normal (Normal (..), normal)
+import Data.Random.Distribution.Uniform (Uniform (..), uniform)
+import Data.Random.Distribution.Gamma (Gamma (..), normal)
 import Data.Random.Source.PureMT as MT
 
 import Control.Monad.State (evalState)
+import Control.Arrow (first)
 
 newtype Prob = Prob { toDouble :: Double } deriving (Show, Eq, Ord, Num, Fractional, Real, RealFrac, Floating, Random)
+
+newtype Explicit a = Explicit { toList :: [(a, Prob)] }
+
+instance Functor Explicit where
+  fmap h = Explicit . fmap (first h) . toList 
+
+instance Applicative Explicit where
+  pure x = Explicit [(x, 1)]
+  (Explicit fs) <*> (Explicit as) = Explicit $ [(f a, pf * pa) | (f, pf) <- fs, (a, pa) <- as]
+
+instance Monad Explicit where
+  return = pure
+  (Explicit as) >>= f = Explicit [ (a, pa * pb) | (a, pa) <- as, (b, pb) <- toList (f a)] 
+
 
 class Sampleable d where
   sample :: RandomGen g => g -> d a -> a
@@ -43,7 +60,6 @@ instance Sampleable Dist where
   sample g (Primitive da) = sample g da
   sample g (Conditional f da) = undefined
   
-
 sampleState = runState . R.sampleRVar
 
 -- NB these Sampleable instances all throw away the new generator.  What's up with that?
@@ -57,6 +73,9 @@ instance R.Distribution Uniform a => Sampleable (Uniform a) where
 instance R.Distribution Normal a => Sampleable (Normal a) where
   sample g (Normal m sd) = fst $ sampleState (normal m sd) g
 
+instance R.Distribution Gamma a => Sampleable (Gamma a) where
+  sample g (Gamma x y) = fst $ sampleState (gamma x y) g
+
 uniformList :: [a] -> Dist a
 uniformList = Primitive
 
@@ -66,6 +85,7 @@ uniformLoHi lo hi = Primitive (Uniform lo hi)
 normal :: (Real a, R.Distribution Normal a) => a -> a -> Dist a
 normal mean sd = Primitive (Normal mean sd) 
 
-
+gamma :: (Floating a, Ord a, Distribution Normal a, Distribution StdUniform a) => a -> a -> Dist a
+gamma x y = Primitive (Gamma x y)
 
 
